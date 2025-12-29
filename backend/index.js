7491b930d83e693fdbbe4021ce553334f6a59e86
Shubham Agarwal <shubham.agarwal@cubastion.com>
2025-12-29 16:17:05 +0530

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from Backend API 🚀" });
+});
+
+app.listen(5000, "0.0.0.0", () => {
+  console.log("Backend running on port 5000");
+});
---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+
+import { useEffect, useState } from "react";
+
+function App() {
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    fetch("http://backend:5000/api")
+      .then(res => res.json())
+      .then(data => setMsg(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>Client-Server Docker App</h1>
+      <p>{msg}</p>
+    </div>
+  );
+}
+
+export default App;
