01b12d89c4c206fad0e34e993b5a43f189fbbe3e
Shubham Agarwal <shubham.agarwal@cubastion.com>
2026-01-07 12:03:53 +0530

    Added nginx flow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,38 @@
 
+// import { useEffect, useState } from "react";
+
+// function App() {
+//   const [msg, setMsg] = useState("");
+
+//   useEffect(() => {
+//     fetch("http://backend:5000/api")
+//       .then(res => res.json())
+//       .then(data => setMsg(data.message));
+//   }, []);
+
+//   // let temp="unused";
+
+//   return (
+//     <div>
+//       <h1>Client-Server Docker App</h1>
+//       <p>{msg}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import { useEffect, useState } from "react";
 
 function App() {
   const [msg, setMsg] = useState("");
 
   useEffect(() => {
-    fetch("http://backend:5000/api")
+    fetch("/api")
       .then(res => res.json())
-      .then(data => setMsg(data.message));
+      .then(data => setMsg(data.message))
+      .catch(err => console.error(err));
   }, []);
-
-  // let temp="unused";
 
   return (
     <div>
